e2eedc9916a660c8105121a2bfa326c1c8c065ab
Matchobas <maalexandria@hotmail.com>
2023-01-31 21:46:09 -0300

    Creating the first request

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get('/', () => {
-  return "Hello NLW Setup for 2023";
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany();
+  return habits;
 });
 
 app.listen({ port: 3333 }, () => {
