d9b2e91b5650cc819521d56fa131151bbee8ca45
Matchobas <maalexandria@hotmail.com>
2023-02-04 23:08:45 -0300

    Adding validation with zod

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { FastifyInstance } from 'fastify';
 import { prisma } from './lib/prisma';
+import { z } from 'zod';
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const habits = await prisma.habit.findMany();
-    return habits;
+  app.post('/habits', async (request, response) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6))
+    })
+    
+    const { title, weekDays } = createHabitBody.parse(request.body);
   });
 }
