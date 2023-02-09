a7ca03b8d6d615b49a350c1ed0fde904b8d8be3c
Matchobas <maalexandria@hotmail.com>
2023-02-08 23:37:57 -0300

    Untoggle habit route

--- src/routes.ts
+++ src/routes.ts
@@ -91,11 +91,28 @@ export async function appRoutes(app: FastifyInstance) {
       });
     }
 
-    await prisma.dayHabit.create({
-      data: {
-        day_id: day.id,
-        habit_id: id
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id
+        }
       }
     });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id
+        }
+      })
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id
+        }
+      });
+    }
   });
 }
